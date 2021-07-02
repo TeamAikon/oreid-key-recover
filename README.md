76c4bcad94f120e248f7e1f81c38e01fc63b251a
Tray Lewin <tray.lewin@live.com>
2021-07-02 10:11:27 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # oreid-key-recover
 Recover keys from oreid key backup file
+
+# Setup
+npm install --global pkg
+
+# To build executable packages
+npm run build
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+
+async function start() {
+  console.log('App typescript is running...')
+}
+
+( async function() {
+  await start()
+} )();
