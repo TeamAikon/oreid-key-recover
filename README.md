28586d5cc408d470c735315bd6b028863ffef912
basarrcan <basarrcan@gmail.com>
2021-07-02 21:42:12 +0300

    add decrypt

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ npm install --global pkg
 
 # To build executable packages
 npm run build
+
+If you get 'FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory'
+Try running on cli:
+
+export NODE_OPTIONS=--max_old_space_size=4096 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
 import path from 'path';
+import * as fs from 'fs';
+import { ChainFactory, ChainType } from '@open-rights-exchange/chainjs'
 
-async function start() {
-  console.log('App typescript is running...')
+function getChain(chain: string) {
+  switch (chain) {
+    case 'eos':
+      return new ChainFactory().create(ChainType.EosV2, [])
+    case 'ethereum':
+      return new ChainFactory().create(ChainType.EthereumV1, [])
+    case 'algorand':
+      return new ChainFactory().create(ChainType.AlgorandV1, [])
+    default:
+      throw new Error('Invalid chain argument passed')
+  }
+
 }
 
-( async function() {
+
+async function start() {
+  const filePath = process.argv[2]
+  const password = process.argv[3] // TODO: make convert it to ask input runtime
+  const rawdata = fs.readFileSync(filePath);
+  const accountJson = JSON.parse(rawdata.toString())
+  const { accountName, keys } = accountJson
+  console.log('Recovery running for account: ', accountName)
+
+  const decrypted = []
+  for (const key of keys) {
+    const { chain, privayeKeys } = key
+    const chainjs = getChain(chain)
+    const decryptedKey = privayeKeys.map((pk: any) => {
+      const { privateKeyEncryped, privateKeyEncryptionSalt } = pk
+      const decryptedPrivateKey = chainjs.decryptWithPassword(privateKeyEncryped, password, { salt: privateKeyEncryptionSalt })
+      return { ...pk, decryptedPrivateKey }
+    })
+    decrypted.push(decryptedKey)
+  }
+  console.log(decrypted)
+
+}
+
+(async function () {
   await start()
-} )();
+})();
