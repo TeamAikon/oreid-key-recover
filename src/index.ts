99e683728565c4cabfe47c2061b12d5749fe8aa4
Tray Lewin <tray.lewin@live.com>
2021-07-02 10:32:52 -0700

    add psuedocode

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
 import path from 'path';
 
+
+// oreid-backup.json
+// accountName: 'ore...', // ore wallet account
+// keys: [ 
+//   {
+//     chain: 'eos',
+//     privateKeys: [
+//       { account: chainAccount, 
+//         encrytpedKey: { credentials from DB - publicKey, privateKeyEncrypted, privateKeyEncryptionSalt }
+//       }  
+//     ]
+//   }
+// ]
+
 async function start() {
+
+  // require command-line switch for --network (-n) 'algorand', 'eos', 'ethereum'
+  // Prompt for user to enter password
+  // load credential from local file - ./oreid-backup.json
+  // loop through backup - create chainjs chain for chain type
+  // -- foreach credential, chain.decryptWithPassword using password entered and salt in credential
+  // for each key, display chain, account (chainAccount), public key, private key 
+
+  // Enter you wallet password: nnnn
+  // Your Keys:
+  //    {chain}: account: {chainAccoutn}, public: {publicKey}, private: {privateKey}
+
   console.log('App typescript is running...')
 }
 
